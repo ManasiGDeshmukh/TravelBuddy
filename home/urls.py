2937e82f40f9503e98d5adf7708ec15d9e3e190f
Manasi Deshmukh <115893690+ManasiGDeshmukh@users.noreply.github.com>
2023-05-01 17:01:48 +0530

    Add files via upload

--- home/urls.py
+++ home/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.index, name='home'),
     path("about", views.about, name='about'),
     path("contact", views.contact, name='contact'),
-    path("packages", views.packages, name='packages')
+    path("packages", views.packages, name='packages'),
+    path("packages/<str:pkid>", views.packageDetails, name='packageDetails')
 ]
